b002ba03e0430a2f5a60d31595568a94abee6134
Chris Hunter <orkem2@gmail.com>
2017-09-25 15:49:06 +0100

    adds OpeningList component

---
+++ client/src/components/OpeningList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import Opening from './Opening';
+
+class OpeningList extends React.Component{
+
+  render(){
+    return(
+        <p>Hello</p>
+      )
+  }
+
+}
+
+export default OpeningList;
--- client/src/components/OpeningMovies.jsx
+++ client/src/components/OpeningMovies.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
-// import OpeningList from './OpeningList';
+import OpeningList from './OpeningList';
 
 class OpeningMovies extends React.Component{
 
   render(){
     return(
-      <p>Hello</p>
+      <section className="opening-movies">
+        <h3>UK Opening This Week</h3>
+        <OpeningList/>
+        <p><a href="http://www.imdb.com/calendar/?region=gb&pf_rd_m=A2FGELUUNOQJNL&pf_rd_p=1970868962&pf_rd_r=0MJG80K9DGZ1BSKH245V&pf_rd_s=right-2&pf_rd_t=15061&pf_rd_i=homepage&ref_=hm_otw_sm">See more opening this week.</a></p>
+      </section>
       )
   }
 }
